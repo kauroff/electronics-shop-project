c33a9971bdf4dbe3ea0df5cceb9a1a84f5e3e85e
Ivan Kaurov <crazyanimalcz@gamil.com>
2023-08-24 18:10:30 +0300

    Finished first homework

--- src/item.py
+++ src/item.py
@@ -13,18 +13,20 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        pass
+        self.name = name
+        self.price = price
+        self.quantity = quantity
 
     def calculate_total_price(self) -> float:
         """
-        Рассчитывает общую стоимость конкретного товара в магазине.
+        Рассчитывает общую стоимость конкретного товара в магазине. Без учета скидки.
 
         :return: Общая стоимость товара.
         """
-        pass
+        return self.price*self.quantity
 
     def apply_discount(self) -> None:
         """
         Применяет установленную скидку для конкретного товара.
         """
-        pass
+        self.price = self.price*self.pay_rate
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,26 @@
+from src.item import Item
+
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Ноутбук", 20000, 5)
+
+# TestCase#0 Pay_rate
+assert Item.pay_rate == 1.0
+
+# TestCase#1 Name
+assert item1.name == 'Смартфон'
+assert item2.name == 'Ноутбук'
+
+# TestCase#2 Price
+assert item1.price == 10000
+assert item2.price == 20000
+
+# TestCase#3 Quantity
+assert item1.quantity == 20
+assert item2.quantity == 5
+
+# TestCase#4 Function total price
+assert item1.calculate_total_price() == float(10000*20)
+assert item2.calculate_total_price() == float(20000*5)
+
