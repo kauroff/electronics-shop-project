e65d37c9b61e8cfe0b3604ac2f171771e7e2505a
Ivan Kaurov <crazyanimalcz@gamil.com>
2023-08-24 18:14:47 +0300

    Finished first homework

--- tests/test_item.py
+++ tests/test_item.py
@@ -2,25 +2,25 @@ from src.item import Item
 
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 
-item1 = Item("Яблоко", 50, 100)
-item2 = Item("Слива", 70, 50)
+item1 = Item("Монитор", 15000, 70)
+item2 = Item("Мышка", 1000, 120)
 
 # TestCase#0 Pay_rate
 assert Item.pay_rate == 1.0
 
 # TestCase#1 Name
-assert item1.name == 'Яблоко'
-assert item2.name == 'Слива'
+assert item1.name == 'Монитор'
+assert item2.name == 'Мышка'
 
 # TestCase#2 Price
-assert item1.price == 50
-assert item2.price == 70
+assert item1.price == 15000
+assert item2.price == 120
 
 # TestCase#3 Quantity
-assert item1.quantity == 100
-assert item2.quantity == 50
+assert item1.quantity == 70
+assert item2.quantity == 120
 
 # TestCase#4 Function total price
-assert item1.calculate_total_price() == float(50*100)
-assert item2.calculate_total_price() == float(70*50)
+assert item1.calculate_total_price() == float(15000*70)
+assert item2.calculate_total_price() == float(120*120)
 
