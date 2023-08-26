7c5550c231748a2d46de734db5ede8654e6564e1
Ivan Kaurov <crazyanimalcz@gamil.com>
2023-08-26 22:50:14 +0300

    Finished second homework

--- tests/test_item.py
+++ tests/test_item.py
@@ -4,6 +4,7 @@ from src.item import Item
 
 item1 = Item("Монитор", 15000, 70)
 item2 = Item("Мышка", 1000, 120)
+item3 = Item("Заглушка", 1000, 120)
 
 # TestCase#0 Pay_rate
 assert Item.pay_rate == 1.0
@@ -14,7 +15,7 @@ assert item2.name == 'Мышка'
 
 # TestCase#2 Price
 assert item1.price == 15000
-assert item2.price == 120
+assert item2.price == 1000
 
 # TestCase#3 Quantity
 assert item1.quantity == 70
@@ -22,5 +23,21 @@ assert item2.quantity == 120
 
 # TestCase#4 Function total price
 assert item1.calculate_total_price() == float(15000*70)
-assert item2.calculate_total_price() == float(120*120)
+assert item2.calculate_total_price() == float(1000*120)
+
+# TestCase#5 name.getter
+assert item1.name == 'Монитор'
+
+# TestCase#6 name.setter
+item1.name = 'Смартфон'
+assert item1.name == 'Смартфон'
+item3.name = 'Беспроводная зарядка'
+assert item3.name == 'Беспроводн'
+
+# TestCase#7 Count positions
+Item.instantiate_from_csv('../src/items.csv')
+assert len(Item.all) == 5
+
+# TestCase#8 Equal
+assert Item.string_to_number('7.7') == 7
 
